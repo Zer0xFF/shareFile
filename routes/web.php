8334b56cfa340ef6e76b6ae7d739b09cb86f2b99
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2021-08-26 13:30:46 +0100

    initial upload/download routes

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::put('/{name}', function(Request $request, $name)
+{
+    $baseUrl = env('APP_URL');
+    $content = $request->getContent();
+    $fileHash = sha1($content);
+
+    // add hash to name to avoid collisions
+    $names = explode('/', $name);
+    $names[count($names) - 1] = $fileHash . '_' . $names[count($names) - 1];
+    $name = implode('/', $names);
+
+    if(!Storage::disk('local')->exists($name))
+        Storage::disk('local')->put($name, $content);
+
+    return "$baseUrl/$name";
+})->where('name', '.*');
+
+Route::get('/{name}', function(Request $request, $filename)
+{
+    if(!Storage::disk('local')->exists($filename))
+        abort(404);
+
+    // remove hash from name
+    $names = explode('/', $filename);
+    $names = explode('_', $names[count($names) - 1]);
+    array_shift($names);
+    $name = implode('_', $names);
+
+    return Storage::disk('local')->download($filename, $name);
+})->where('name', '.*');
