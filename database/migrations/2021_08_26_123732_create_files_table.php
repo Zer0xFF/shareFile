e8da56a41eebee914c0a09b178f5f69f68b8476c
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2021-08-26 13:55:36 +0100

    add file model

---
+++ database/migrations/2021_08_26_123732_create_files_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('path');
+            $table->string('name');
+            $table->integer('ref')->default(1);
+            $table->integer('download_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
