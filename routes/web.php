612609d08478cd73cc39a65c6cd9847f61867a1c
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2021-08-26 13:55:52 +0100

    keep track of changes in database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,25 +28,39 @@ Route::put('/{name}', function(Request $request, $name)
 
     // add hash to name to avoid collisions
     $names = explode('/', $name);
-    $names[count($names) - 1] = $fileHash . '_' . $names[count($names) - 1];
-    $name = implode('/', $names);
+    $name = $fileHash . '_' . $names[count($names) - 1];
+    $names[count($names) - 1] = $name;
+    $path = implode('/', $names);
 
-    if(!Storage::disk('local')->exists($name))
-        Storage::disk('local')->put($name, $content);
+    if(!Storage::disk('local')->exists($path))
+    {
+        $file = File::create(compact('name', 'path'));
+        Storage::disk('local')->put($path, $content);
+    }
+    else
+    {
+        $file = File::where(compact('name', 'path'))->get()->first();
+        $file->ref += 1;
+        $file->save();
+    }
 
     return "$baseUrl/$name";
 })->where('name', '.*');
 
-Route::get('/{name}', function(Request $request, $filename)
+Route::get('/{name}', function(Request $request, $path)
 {
-    if(!Storage::disk('local')->exists($filename))
+    if(!Storage::disk('local')->exists($path))
         abort(404);
 
     // remove hash from name
-    $names = explode('/', $filename);
+    $names = explode('/', $path);
+    $hashedName = $names[count($names) - 1];
     $names = explode('_', $names[count($names) - 1]);
     array_shift($names);
     $name = implode('_', $names);
 
-    return Storage::disk('local')->download($filename, $name);
+    $file = File::where(['name' => $hashedName, 'path' => $path])->get()->first();
+    $file->download_count += 1;
+    $file->save();
+    return Storage::disk('local')->download($path, $name);
 })->where('name', '.*');
