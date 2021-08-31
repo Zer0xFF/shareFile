f2f182ef6e2e3df7fb24ee226b1f3913597b5f1f
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2021-08-31 14:08:09 +0100

    basic file browser layout

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function()
+{
+    $files = File::all();
+    return view('index', compact('files'));
 });
 
 
